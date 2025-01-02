1bc8dab045883e148de66f30e2586e47af6a593b
Juyi Kim <45526515+jjuyii49@users.noreply.github.com>
2025-01-02 18:12:57 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,5 @@
+select fi.ID, fni.FISH_NAME, fi.LENGTH
+from FISH_INFO fi join FISH_NAME_INFO fni
+on fi.FISH_TYPE = fni.FISH_TYPE
+where (fi.FISH_TYPE, fi.LENGTH) in (select FISH_TYPE, MAX(LENGTH) from FISH_INFO group by FISH_TYPE)
+order by fi.ID
