ffa99ddd7f3845087ee73062c2e879c6053beba8
Juyi Kim <45526515+jjuyii49@users.noreply.github.com>
2025-02-17 23:01:17 +0900

    [Silver III] Title: N과 M (4), Time: 100 ms, Memory: 17168 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        sb = new StringBuilder();
+        comb(1, 0, new ArrayList<>());
+
+        System.out.println(sb);
+    }
+
+    private static void comb(int idx, int count, ArrayList<Integer> list) {
+        if(count == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(list.get(i)).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = idx; i <= N; i++) {
+            list.add(i);
+            comb(i, count + 1, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
