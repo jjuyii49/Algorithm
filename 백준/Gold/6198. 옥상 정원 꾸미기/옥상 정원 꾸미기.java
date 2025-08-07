3f612e85c7a65e976320aadc3709904b1363274f
Juyi Kim <45526515+jjuyii49@users.noreply.github.com>
2025-08-07 17:21:04 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 332 ms, Memory: 23456 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
 
@@ -8,22 +9,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine().trim());
 
-        int[] buildings = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        long answer = 0;
+
         for(int i = 0; i < N; i++) {
             int height = Integer.parseInt(br.readLine().trim());
 
-            buildings[i] = height;
-        }
-
-        long answer = 0;
-        for(int i = 0; i < N; i++) {
-            int temp = buildings[i];
-
-            for(int j = i + 1; j < N; j++) {
-                if(temp <= buildings[j]) break;
-
-                answer++;
+            while(!stack.isEmpty() && stack.peek() <= height) {
+                stack.pop();
             }
+
+            answer += stack.size();
+            stack.push(height);
         }
 
         System.out.println(answer);
